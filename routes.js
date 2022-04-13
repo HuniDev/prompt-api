0320a10a980bb090f27eeb8391aaf190995d12e3
annashiii <bhogan.gths@gmail.com>
2022-04-12 20:23:24 -0400

    get multiple prompts

--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import Data from './model/model.js';
 const router = express.Router();
 
@@ -16,6 +17,24 @@ router.get('/random', async (req, res) => {
 		const count = await Data.countDocuments();
 		const random = Math.floor(Math.random() * count + 1);
 		const data = await Data.find({ id: random }, { _id: 0 });
+		res.send(data);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+
+router.get('/10', async (req, res) => {
+	try {
+		const data = await Data.aggregate([{ $sample: { size: 10 } }]);
+		res.send(data);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+});
+
+router.get('/5', async (req, res) => {
+	try {
+		const data = await Data.aggregate([{ $sample: { size: 5 } }]);
 		res.send(data);
 	} catch (error) {
 		res.status(500).json({ message: error.message });
